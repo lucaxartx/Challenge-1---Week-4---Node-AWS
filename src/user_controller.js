27de08f879d4ad2e39ef7a82f1936e3c62e3ff80
lucaxartx <ujude1000@gmail>
2023-06-28 20:55:12 -0700

    feat: added create user controller

---
+++ src/user_controller.js
@@ -0,0 +1,64 @@
+const CustomError = require('../errors');
+const User = require('../model/user_model');
+
+const createNewUser = async (req, res)=>{
+    const {name, birthday, phone, email,
+      password, passwordConfirm, zipCode,
+      street,number,neighbourhood,city
+     } =  req.body;
+
+     if(!name){
+        throw new CustomError.NotFoundError('Please provide name');
+     }
+ 
+     if(!birthday){
+        throw new CustomError.NotFoundError('Please provide birthday');
+     }
+ 
+     if(!phone){
+        throw new CustomError.NotFoundError('Please provide phone');
+     }
+ 
+     if(!email){
+        throw new CustomError.NotFoundError('Please provide email');
+     }
+ 
+     if(!password){
+        throw new CustomError.NotFoundError('Please provide password');
+     }
+ 
+     if(!passwordConfirm){
+        throw new CustomError.NotFoundError('Please provide passwordConfirm');
+     }
+ 
+     if(!zipCode){
+        throw new CustomError.NotFoundError('Please provide zipCode');
+     }
+ 
+     if(!street){
+        throw new CustomError.NotFoundError('Please provide street');
+     }
+ 
+     if(!number){
+        throw new CustomError.NotFoundError('Please provide number');
+     }
+ 
+     if(!city){
+        throw new CustomError.NotFoundError('Please provide city');
+     }
+ 
+     if(!neighbourhood){
+        throw new CustomError.NotFoundError('Please provide neighbourhood');
+     }
+
+        if(password !== passwordConfirm){
+            throw new CustomError.BadRequestError('Passwords do not match');
+        }
+
+        const user = await User.create({name, birthday, phone, email,
+            password, passwordConfirm,zipcode: zipCode,street,number,neighbourhood,city});
+
+        res.status(200).json({
+            user });
+}
+module.exports={createNewUser}
